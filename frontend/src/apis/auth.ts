08634d52987c245901c6f5515469dabc8297f5da
stoqel33 <stochel202@interia.pl>
2022-06-16 11:53:38 +0200

    add api list

---
+++ frontend/src/apis/auth.ts
@@ -0,0 +1,6 @@
+import { IApiUrls } from 'apis/interface';
+
+export const authApi: IApiUrls = {
+    register: 'register',
+    login: '/login',
+};
---
+++ frontend/src/apis/interface.ts
@@ -0,0 +1,3 @@
+export interface IApiUrls {
+    [key: string]: string;
+}
